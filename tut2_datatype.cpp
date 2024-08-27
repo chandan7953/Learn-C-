395f120d0ab858baca03581756640f0fd7f889d1
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2024-08-27 19:09:22 +0530

    Data type

---
+++ tut2_datatype.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // syntax
+    // datatype variableName = value;
+
+    int a = 5;
+    int A = 10;
+
+    cout << a << "\t" << A << endl;
+
+    float c = 45.4;
+    cout << c << endl;
+
+    double d = 45;
+    cout << d << endl;
+
+    char ch = 'a';
+    cout << ch << endl;
+
+    string str = "Chandan Singh";
+    cout << str << endl;
+
+    return 0;
+}
---
+++ tut3_cin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number ";
+    cin >> a;
+    cout << a << endl;
+    cout << sizeof(a);
+
+    // string str;
+    // // cin >> str;
+    // getline(cin, str);
+    // cout << str;
+
+    return 0;
+}
